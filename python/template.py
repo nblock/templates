6089300838d14510fa2dd32038fda481bfe47568
nblock <nblock@archlinux.us>
2011-09-13 10:08:58 +0200

    add a python template

---
+++ python/template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+# <project name>
+# author: name <e-mail>
+# license: <license>
+
+#code here
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent autoindent 
